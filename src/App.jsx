ea1707e615e0f44ff267f9d7bf6fa4f353dcfdfb
IsekaiRid <mainrpg62@gmail.com>
2024-07-27 14:12:42 +0700

    gabung

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Card from './Components/Card'
 
 const App = () => {
   
   return (
     <div>
+      <Navbar />
       <Card />
     </div>
   )

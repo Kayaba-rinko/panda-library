2d51e64a0c66d081a683203fb368edafa2844806
DHESVIRA NP <dhesviransehap@gmail.com>
2024-07-27 12:57:18 +0700

    k

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
                 </ul>
                 <form className="d-flex" role="search">
                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                <button className="btn btn-outline-success" type="submit">Search</button>
+                <button className="btn btn-outline-success" type="submit">Searchq</button>
                 </form>
             </div>
             </div>
